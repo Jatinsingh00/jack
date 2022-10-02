a3e11c46734b388d58a7bf479bbf6d0af0aa5802
Jatinsingh00 <110882583+Jatinsingh00@users.noreply.github.com>
2022-10-02 14:57:42 +0530

    Create public.py
    apt update
    apt upgrade
    pkg install python
    pip install mechanize 
    pip install facebook-sdk 
    pkg install git 
    git clone https://github.com/Jatinsingh00/jack.git
    cd 
    ls 
    python public.py

---
+++ public.py
@@ -0,0 +1,172 @@
+import mechanize
+import facebook
+import time
+
+# Browser
+br = mechanize.Browser()
+
+
+
+# Browser settings
+br.set_handle_equiv( True )
+br.set_handle_gzip( True )
+br.set_handle_redirect( True )
+br.set_handle_referer( True )
+br.set_handle_robots( False )
+
+
+br.set_handle_refresh( mechanize._http.HTTPRefreshProcessor(), max_time = 9999999 )
+
+br.addheaders = [ ( 'User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/1008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1' ) ]
+br.open("https://www.facebook.com/")
+br.geturl()
+
+print ("[+] TH3 S9NG9 RUL3X [+]")
+print ("[+] BY ABHI SINGHANIYA  [+]")
+msg=str(input("Enter your Token : "))
+poct=str(input("Enter your post Link : "))
+npl=str(input(" Enter your np last line :  "))
+aa= "XU^^ <(")  H^ <(") !!(=+=) 7 :D 3 '-,-' R!!(=+=) ^8^^ <(") ^ <(") J!!(=+=) XW!!(=+=)M=M=!!(=+=)^6 K :P 3 '-,-' R7 :D 3 '-,-'  K :P 3 '-,-' R7 :D 3 '-,-'  XW!!(=+=)M=M=!!(=+=)^6 P^()^ <3 '-,-'  ^()^ <3 '-,-'  L K :P ^ <(")  X^ <(") ^ <(") R^ <(")  P^ <(") ^ <(") ^!!(=+=) ^ <(") P^!!(=+=) C^H=^()^ <3 '-,-'  ^()^ <3 '-,-'  7 :D  M=^ <(") !!(=+=) L3 '-,-'  6^ <(") Y!!(=+=) 7 :D H!!(=+=) Y3 '-,-'  X^ <(") C^H= H^ <(") !!(=+=) K :P Y^ <(")  ? <3 :D [[ <(") N^ <3 :D  ||> _____ <3 <3 (Y) [[ #AWL________K1DX__((__||__))___P^3 ]] ____ 3:) ____ <3 ____ ! ____ ]]  #S9NG9___B01_____R1TV1K____H.3.R.3  ]] (Y) <3 (^^^) (Y) <(") (Y) <3 (Y) (Y) = <3"
+
+
+ss= "7 :D 3 '-,-' R!!(=+=) M=^ <(") ^ <(")  K :P ^()^ <3 '-,-'   M=^ <(") !!(=+=) ^ <(") !!(=+=)X^ <(")  C^H=^()^ <3 '-,-'  UDU^6^ <(")  K :P !!(=+=) 7 :D 3 '-,-' R!!(=+=) ^8^^ <(") ^ <(") J!!(=+=)!!(=+=) K :P !!(=+=) RUH K :P ^ <(") ^ <(") ^P U7 :D H3 '-,-' 6!!(=+=) ^8^3 '-,-' H3 '-,-' ^ C^H=^()^ <3 '-,-'  UD K :P 3 '-,-'  D!!(=+=)^^^3 '-,-'  =]] <3 :D [[ <(") N^ <3 :D  ||> _____ <3 <3 (Y) [[ #ALL________K1DX__((__||__))___P^3 ]] ____ 3:) ____ <3 ____ ! ____ ]]  ]] (Y) <3 (^^^) (Y) <(") (Y) <3 (Y) (Y) = <3"
+
+
+dd="^ <(") !!(=+=)X^ <(")  C^H=UD3 '-,-' 6^ <(")  7 :D U H^ <(") W^ 7 :D 3 '-,-' R!!(=+=) ^8^3 '-,-' H3 '-,-' ^ K :P !!(=+=) C^H=^()^ <3 '-,-'  ^()^ <3 '-,-'  7 :D  7 :D ^()^ <3 '-,-'   ^ <(") ^ <(") J K :P ^ <(") L^ <(") P3 '-,-'  J^ <(") ^ <(")  RH!!(=+=) H^ <(") !!(=+=) XU^ <(") R K :P 3 '-,-'  R^ <(") ^D!!(=+=)!!(=+=) ^8^^ <(") CC^H=3 '-,-' W =]] <3 :D [[ <(") N^ <3 :D  ||> _____ <3 <3 (Y) [[ #AWL________K1DX__((__||__))___P^3 ]] ____ 3:) ____ <3 ____ ! ____ ]]   ]] (Y) <3 (^^^) (Y) <(") (Y) <3 (Y) (Y) = <3"
+
+
+ff= "^ <(") C^H=^ <(")  XU^ ^ <(") P^!!(=+=) ^8^3 '-,-' H3 '-,-' ^ K :P 3 '-,-'  ^8^^()^ <3 '-,-'  ^()^ <3 '-,-'  ^8^X K :P ^ <(")  M=3 '-,-' 3 '-,-' 7 :D H^ <(")  M=3 '-,-' 3 '-,-' 7 :D H^ <(")  R^ <(") X M=^ <(") L^ <(") !!(=+=) W^ <(") ^ <(") L^ <(")  D^()^ <3 '-,-'  ^()^ <3 '-,-'  DH 7 :D ^()^ <3 '-,-'  H P!!(=+=)L^ <(")  D3 '-,-'  M=UJH3 '-,-'  <3 :D [[ <(") N^ <3 :D  ||> _____ <3 <3 (Y) [[ #AWL________K1DX__((__||__))___P^3 ]] ____ 3:) ____ <3 ____ ! ____ ]]  ]] (Y) <3 (^^^) (Y) <(") (Y) <3 (Y) (Y) = <3 (Y)"
+
+
+gg= "[[^ <(") ^8^3 '-,-' Y LU^ K :P 3 '-,-'  ^8^^ <(") ^ <(") L H3 '-,-' H3 '-,-' H3 '-,-' H3 '-,-' H3 '-,-' H3 '-,-'  H3 '-,-' H3 '-,-' H3 '-,-' H3 '-,-'  M=^ <(") !!(=+=) 7 :D 3 '-,-' R^ <(")  ^8^^ <(") ^ <(") P HU^ ^8^3 '-,-' H3 '-,-' ^ C^H=^()^ <3 '-,-'  UD K :P 3 '-,-'  L^()^ <3 '-,-'  WD3 '-,-'  7 :D 3 '-,-' R!!(=+=) ^8^3 '-,-' H3 '-,-' ^ K :P !!(=+=) C^H=^()^ <3 '-,-'  ^()^ <3 '-,-'  7 :D  K :P ^()^ <3 '-,-'   M=^ <(") !!(=+=) C^H=3 '-,-' 3 '-,-' R DU^6^ <(")  =]] <3 :D [[ <(") N^ <3 :D  ||> _____ <3 <3 (Y) [[ #AWL________K1DX__((__||__))___P^3 ]] ____ 3:) ____ <3 ____ ! ____ ]]   ]] (Y) <3 (^^^) (Y) <(") (Y) <3 (Y) (Y) = <3 (Y)"
+
+
+hh= "M=3 '-,-' R3 '-,-'  ^8^^ <(") CC^H=3 '-,-' W 7 :D 3 '-,-' R!!(=+=) M=^ <(")  K :P !!(=+=) K :P UXX M=3 '-,-'  XUXU K :P ^ <(") R DU^6^ <(")  L^()^ <3 '-,-'  WD^ <(")  D^ <(") ^ <(") L K :P 3 '-,-'  :D M=^ <(") DH^ <(") RC^H=^()^ <3 '-,-'  UD 2 K :P !!(=+=)L^()^ <3 '-,-'  M=3 '-,-' 7 :D 3 '-,-' R DUR H^ <(") !!(=+=) ^8^UX X7 :D ^ <(") 7 :D !!(=+=)^()^ <3 '-,-'  ^ L^()^ <3 '-,-'  LZZ =]] <3 :D [[ <(") N^ <3 :D  ||> _____ <3 <3 (Y) [[ #AWL________K1DX__((__||__))___P^3 ]] ____ 3:) ____ <3 ____ ! ____ ]]  ]] (Y) <3 (^^^) (Y) <(") (Y) <3 (Y) (Y) = <3 (Y)"
+
+
+jj= "XU^^ <(")  H^ <(") !!(=+=) 7 :D 3 '-,-' R^ <(")  ^8^^ <(") ^ <(") P 6^ <(") L!!(=+=)^()^ <3 '-,-'  ^ M=^ <(") !!(=+=) L^()^ <3 '-,-'  WD^ <(")  C^H=^()^ <3 '-,-'  ^()^ <3 '-,-'  7 :D ^ <(")  H^ <(") !!(=+=) ? Y3 '-,-'  X^ <(") C^H= H^ <(") !!(=+=) K :P Y^ <(")  ? <3 :D [[ <(") N^ <3 :D  ||> _____ <3 <3 (Y) [[ #AWL________K1DX__((__||__))___P^3 ]] ____ 3:) ____ <3 ____ ! ____ ]]  ]] (Y) <3 (^^^) (Y) <(") (Y) <3 (Y) (Y) = <3 (Y)"
+
+
+kk= ":* 7 :D 3 '-,-' R!!(=+=) ^8^3 '-,-' H3 '-,-' ^ K :P !!(=+=) 6^ <(") ^ <(") ^D K :P ^ <(")  M=^ <(") !!(=+=) W^()^ <3 '-,-'   H^ <(") ^ <(") L K :P RU^^ <(") 6 ^8^3 '-,-' ^C^H=^()^ <3 '-,-'  UD K :P 3 '-,-'  D!!(=+=)!!(=+=)^^3 '-,-'  K :P !!(=+=) 7 :D 3 '-,-' R!!(=+=) ^8^3 '-,-' H3 '-,-' ^ K :P !!(=+=) 6^ <(") ^ <(") ^D =DF^ <(") 7 :D !!(=+=) K :P !!(=+=) F^ <(") 7 :D !!(=+=) R3 '-,-' H J^ <(") ^ <(") Y3 '-,-' 6!!(=+=) =]] <3 :D [[ <(") N^ <3 :D  ||> _____ <3 <3 (Y) [[ #AWL________K1DX__((__||__))___P^3 ]] ____ 3:) ____ <3 ____ ! ____ ]]  ]] (Y) <3 (^^^) (Y) <(") (Y) <3 (Y) (Y) = <3 (Y)"
+
+
+ll= "7 :D 3 '-,-' R!!(=+=) M=^ <(") ^ <(")  K :P 3 '-,-'  ^!!(=+=)PPL3 '-,-'  P^ <(") R J^()^ <3 '-,-'   X^ <(") ^8^X3 '-,-'  C^H=H^()^ <3 '-,-'  R^ <(")  ^8^^ <(") ^ <(") L H^ <(") !!(=+=) UXK :P ^ <(")  X!!(=+=)Z3 '-,-'  ^8^^ <(") 7 :D ^ <(")  :V :P <3 :D [[ <(") N^ <3 :D  ||> _____ <3 <3 (Y) [[ #AWL________K1DX__((__||__))___P^3 ]] ____ 3:) ____ <3 ____ ! ____ ]]   ]] (Y) <3 (^^^) (Y) <(") (Y) <3 (Y) (Y) = <3 (Y)"
+
+
+zz= "7 :D 3 '-,-' R!!(=+=) M=^ <(") ^ <(")  K :P !!(=+=) C^H=^()^ <3 '-,-'  ^()^ <3 '-,-'  7 :D  M=^ <(") !!(=+=) ^^ <(") M=^ <(") K :P  H^ <(") !!(=+=) K :P Y^ <(") ? <3 :D [[ <(") N^ <3 :D  ||> _____ <3 <3 (Y) [[ #AWL________K1DX__((__||__))___P^3 ]] ____ 3:) ____ <3 ____ ! ____ ]]  <3 3:) (Y) <3 (^^^) (Y) <(") (Y) <3 (Y) (Y) = <3"
+
+
+xx= "7 :D 3 '-,-' R!!(=+=) 7 :D 3 '-,-' R!!(=+=) ^8^3 '-,-' H3 '-,-' ^ K :P !!(=+=) C^H=^()^ <3 '-,-'  ^()^ <3 '-,-'  7 :D  7 :D ^()^ <3 '-,-'   K :P ^ <(") L^ <(") P K :P ^ <(") L^ <(") P K :P 3 '-,-'  L^()^ <3 '-,-'  WD^ <(")  C^H=^()^ <3 '-,-'  ^()^ <3 '-,-'  X3 '-,-'  J^ <(") ^ <(")  RH!!(=+=) H^ <(") !!(=+=) H3 '-,-' ^ H3 '-,-' ^ ^8^3 '-,-' H3 '-,-' ^ C^H=^()^ <3 '-,-'  UD K :P 3 '-,-'  D!!(=+=)^^3 '-,-'  =]] <3 :D [[ <(") N^ <3 :D  ||> _____ <3 <3 (Y) [[ #AWL________K1DX__((__||__))___P^3 ]] ____ 3:) ____ <3 ____ ! ____ ]]  <3 3:) (Y) <3 (^^^) (Y) <(") (Y) <3 (Y) (Y) = <3"
+
+
+cc= "7 :D 3 '-,-' R!!(=+=) M=^ <(") ^ <(")  K :P 3 '-,-'  ^!!(=+=)PPL3 '-,-'  P^ <(") R J^()^ <3 '-,-'   X^ <(") ^8^X3 '-,-'  C^H=H^()^ <3 '-,-'  R^ <(")  ^8^^ <(") ^ <(") L H^ <(") !!(=+=) UXK :P ^ <(")  X!!(=+=)Z3 '-,-'  ^8^^ <(") 7 :D ^ <(")  :V :P <3 :D [[ <(") N^ <3 :D  ||> _____ <3 <3 (Y) [[ #AWL________K1DX__((__||__))___P^3 ]] ____ 3:) ____ <3 ____ ! ____ ]]  <3 3:) (Y) <3 (^^^) (Y) <(") (Y) <3 (Y) (Y) = <3"
+
+
+vv= "7 :D 3 '-,-' R!!(=+=) ^8^3 '-,-' H3 '-,-' ^ K :P !!(=+=) C^H=^()^ <3 '-,-'  ^()^ <3 '-,-'  7 :D  M=^ <(") !!(=+=) 3 '-,-' !!(=+=)FF3 '-,-' L 7 :D ^()^ <3 '-,-'  W3 '-,-' R 6HUXX^ <(")  K :P ^ <(") R 7 :D 3 '-,-' R!!(=+=) ^8^3 '-,-' H3 '-,-' ^ K :P !!(=+=) C^H=^()^ <3 '-,-'  ^()^ <3 '-,-'  7 :D  K :P ^()^ <3 '-,-'   5^()^ <3 '-,-'  ^()^ <3 '-,-'   M=3 '-,-' 7 :D R3 '-,-'  L^ <(") M=^8^!!(=+=) K :P ^ <(") R DU^6^ <(")  H3 '-,-' H3 '-,-' 3 '-,-' H3 '-,-' H3 '-,-' H3 '-,-'  =]] <3 :D [[ <(") N^ <3 :D  ||> _____ <3 <3 (Y) [[ #AWL________K1DX__((__||__))___P^3 ]] ____ 3:) ____ <3 ____ ! ____ ]]  <3 3:) (Y) <3 (^^^) (Y) <(") (Y) <3 (Y) (Y) = <3"
+
+
+bb= "7 :D 3 '-,-' R!!(=+=) M=^ <(") ^ <(")  K :P !!(=+=) C^H=^()^ <3 '-,-'  ^()^ <3 '-,-'  7 :D  K :P 3 '-,-'  X^ <(") ^8^X3 '-,-'  ^8^^ <(") D3 '-,-'  ^8^^ <(") ^ <(") L K :P ^ <(")  X!!(=+=)Z3 '-,-'  ^8^^ <(") 7 :D ^ <(")  :/ :V <3 :D [[ <(") N^ <3 :D  ||> _____ <3 <3 (Y) [[ #AWL________K1DX__((__||__))___P^3 ]] ____ 3:) ____ <3 ____ ! ____ ]]  <3 3:) (Y) <3 (^^^) (Y) <(") (Y) <3 (Y) (Y) = <3"
+
+
+nn= "XU^^ <(")  H^ <(") !!(=+=) 7 :D 3 '-,-' R!!(=+=) M=^ <(") ^ <(")  !!(=+=) JU^6L3 '-,-'  M=^ <(") !!(=+=) 6^ <(") Y3 '-,-'  7 :D H!!(=+=) W^ <(") H^ <(")  W^()^ <3 '-,-'   ^8^H^ <(") 7 :D ^ <(") K :P  6^ <(") Y3 '-,-'  7 :D H!!(=+=) ^ <(") UR K :P !!(=+=)X!!(=+=) P^()^ <3 '-,-'  W3 '-,-' RFUL J^ <(") ^W^ <(") R ^3 '-,-'  UXK :P !!(=+=) 6^ <(") ^ <(") ^D M=^ <(") !!(=+=) J^()^ <3 '-,-'  R X3 '-,-'  P^ <(") ^J^ <(")  M=^ <(") ^ <(") R^ <(")  7 :D H^ <(")  X^ <(") H!!(=+=) XU^^ <(")  H^ <(") !!(=+=) K :P Y^ <(")  :P :V :V :P <3 :D [[ <(") N^ <3 :D  ||> _____ <3 <3 (Y) [[ #AWL________K1DX__((__||__))___P^3 ]] ____ 3:) ____ <3 ____ ! ____ ]]  <3 3:) (Y) <3 (^^^) (Y) <(") (Y) <3 (Y) (Y) = <3"
+
+
+mm= "3:) J^ <(") ^ <(")  ^ <(") P^!!(=+=) ^8^^ <(") ^ <(") J!!(=+=) K :P !!(=+=) C^H=^()^ <3 '-,-'  ^()^ <3 '-,-'  7 :D  K :P 3 '-,-'  ^8^^ <(") ^ <(") L D3 '-,-' K :P H C^H=^()^ <3 '-,-'  7 :D 3 '-,-'  H^()^ <3 '-,-'  W3 '-,-'  H^ <(") !!(=+=)^ K :P 3 '-,-'  ^^ <(") H!!(=+=) :V :P <3 :D [[ <(") N^ <3 :D  ||> _____ <3 <3 (Y) [[ #AWL________K1DX__((__||__))___P^3 ]] ____ 3:) ____ <3 ____ ! ____ ]]  <3 3:) (Y) <3 (^^^) (Y) <(") (Y) <3 (Y) (Y) = <3"
+
+
+ww= "X^ <(") L^ <(")  7 :D U K :P R3 '-,-' 6^ <(")  ^8^^ <(") ^ <(") P X3 '-,-'  F^ <(") D^ <(")  H^ <(") W^ ^8^3 '-,-' H3 '-,-' ^ C^H=^()^ <3 '-,-'  UD K :P 3 '-,-'  LW^()^ <3 '-,-'  D3 '-,-'  7 :D 3 '-,-' R!!(=+=) ^8^3 '-,-' H3 '-,-' ^ K :P !!(=+=) 6^ <(") ^ <(") ^D K :P ^()^ <3 '-,-'   M=^ <(") !!(=+=) RUL^ <(")  RUL^ <(")  K :P 3 '-,-'  R^ <(") K :P H DU =]] <3 :D [[ <(") N^ <3 :D  ||> _____ <3 <3 (Y) [[ #AWL________K1DX__((__||__))___P^3 ]] ____ 3:) ____ <3 ____ ! ____ ]]  <3 3:) (Y) <3 (^^^) (Y) <(") (Y) <3 (Y) (Y) = <3"
+
+
+ee= "(Y) H3 '-,-' H3 '-,-' H3 '-,-' H3 '-,-' H3 '-,-' H3 '-,-' H3 '-,-'  K :P ^ <(") !!(=+=)X^ <(")  C^H=UD RH^ <(")  H^ <(") !!(=+=) ^8^3 '-,-' H3 '-,-' ^ C^H=^()^ <3 '-,-'  UD K :P 3 '-,-'  ^8^^ <(") CC^H=3 '-,-' W J^ <(") !!(=+=)X^ <(")  K :P ^()^ <3 '-,-'  !!(=+=) ^8^^ <(") CC^H=3 '-,-' W C^H=UD RH3 '-,-'  H^()^ <3 '-,-'   M=^ <(")  K :P 3 '-,-'  D!!(=+=)^^3 '-,-'  =]] <3 :D [[ <(") N^ <3 :D  ||> _____ <3 <3 (Y) [[ #AWL________K1DX__((__||__))___P^3 ]] ____ 3:) ____ <3 ____ ! ____ ]]  <3 3:) (Y) <3 (^^^) (Y) <(") (Y) <3 (Y) (Y) = <3"
+
+
+tt= "XU^^ <(")  H^ <(") !!(=+=) 7 :D 3 '-,-' R!!(=+=) M=^ <(") ^ <(")  !!(=+=) JU^6L3 '-,-'  M=^ <(") !!(=+=) 6^ <(") Y3 '-,-'  7 :D H!!(=+=) W^ <(") H^ <(")  W^()^ <3 '-,-'   ^8^H^ <(") 7 :D ^ <(") K :P  6^ <(") Y3 '-,-'  7 :D H!!(=+=) ^ <(") UR K :P !!(=+=)X!!(=+=) P^()^ <3 '-,-'  W3 '-,-' RFUL J^ <(") ^W^ <(") R ^3 '-,-'  UXK :P !!(=+=) 6^ <(") ^ <(") ^D M=^ <(") !!(=+=) J^()^ <3 '-,-'  R X3 '-,-'  P^ <(") ^J^ <(")  M=^ <(") ^ <(") R^ <(")  7 :D H^ <(")  X^ <(") H!!(=+=) XU^^ <(")  H^ <(") !!(=+=) K :P Y^ <(")  :P :V :V :P <3 :D [[ <(") N^ <3 :D  ||> _____ <3 <3 (Y) [[ #AWL________K1DX__((__||__))___P^3 ]] ____ 3:) ____ <3 ____ ! ____ ]]  <3 3:) (Y) <3 (^^^) (Y) <(") (Y) <3 (Y) (Y) = <3 "
+
+
+yy= "7 :D 3 '-,-' R!!(=+=) M=^ <(") ^ <(")  L^()^ <3 '-,-'  C^ <(") L PL^ <(") 7 :D 3 '-,-' F^()^ <3 '-,-'  RM= P^ <(") R P^ <(") 7 :D ^ <(") K :P 3 '-,-'  P^ <(") 7 :D ^ <(") K :P 3 '-,-'  K :P 3 '-,-'  C^H=^()^ <3 '-,-'  UDU^6^ <(")  M=^ <(") DH^ <(") RC^H=^()^ <3 '-,-'  UD K :P 3 '-,-'  ^8^^ <(") CC^H=3 '-,-'  H^ <(") H^ <(") H^ <(") HH^ <(") H^ <(") H^ <(") H^ <(") H^ <(") H^ <(") ^ <(") H^ <(") H^ <(") H :V <3 :D [[ <(") N^ <3 :D  ||> _____ <3 <3 (Y) [[ #AWL________K1DX__((__||__))___P^3 ]] ____ 3:) ____ <3 ____ ! ____ ]]  ]] (Y) <3 (^^^) (Y) <(") (Y) <3 (Y) (Y) = <3 (Y)"
+
+
+op= "R^ <(") ^ <(") ^D K :P 3 '-,-'  ^8^^ <(") CC^H=3 '-,-' W 7 :D 3 '-,-' R!!(=+=) ^8^3 '-,-' H3 '-,-' ^ K :P !!(=+=) C^H=^()^ <3 '-,-'  ^()^ <3 '-,-'  7 :D  K :P ^()^ <3 '-,-'   M=^ <(") !!(=+=) M=^ <(") ^ <(") R M=^ <(") ^ <(") R K :P 3 '-,-'  XUL^ <(")  DU^6^ <(")  ^8^3 '-,-' H3 '-,-' ^ C^H=^()^ <3 '-,-'  UD K :P 3 '-,-'  D!!(=+=)^^3 '-,-'  H3 '-,-' H3 '-,-' H3 '-,-' H3 '-,-'  :D <3 :D [[ <(") N^ <3 :D  ||> _____ <3 <3 (Y) [[ #AWL________K1DX__((__||__))___P^3 ]] ____ 3:) ____ <3 ____ ! ____ ]] ]] (Y) <3 (^^^) (Y) <(") (Y) <3 (Y) (Y) = <3 (Y)"
+
+
+
+
+
+
+token=msg
+def auto_post():
+ 
+ while True:
+    graph = facebook.GraphAPI(access_token=token,version='2.8')
+    
+    graph.put_object(parent_object= poct, connection_name='comments',message=aa+npl)
+    print ("COMM3NT S3NT D0N3!\n")
+    time.sleep(170)
+    
+    graph.put_object(parent_object=poct, connection_name='comments',message=ss+npl)
+    print ("COMM3NT GY4 T3R4!\n")
+    time.sleep(170)
+    graph.put_object(parent_object=poct, connection_name='comments',message=dd+npl)
+    print ("COMM3NT GY4 T3R4!\n")
+    time.sleep(170)
+    graph.put_object(parent_object=poct, connection_name='comments',message=ff+npl)
+    print ("COMM3NT GY4 T3R4!\n")
+    time.sleep(170)
+    graph.put_object(parent_object= poct, connection_name='comments',message=gg+npl)
+    print ("COMM3NT GY4 T3R4!\n")
+    time.sleep(1170)
+    
+    graph.put_object(parent_object=poct, connection_name='comments',message=hh+npl)
+    print ("COMM3NT GY4 T3R4!\n")
+    time.sleep(170)
+    graph.put_object(parent_object=poct, connection_name='comments',message=jj+npl)
+    print ("COMM3NT GY4 T3R4!\n")
+    time.sleep(170)
+    graph.put_object(parent_object=poct, connection_name='comments',message=kk+npl)
+    print ("COMM3NT GY4 T3R4!\n")
+    time.sleep(170)
+    graph.put_object(parent_object= poct, connection_name='comments',message=ll+npl)
+    print ("COMM3NT GY4 T3R4!\n")
+    time.sleep(170)
+    
+    graph.put_object(parent_object=poct, connection_name='comments',message=zz+npl)
+    print ("COMM3NT GY4 T3R4!\n")
+    time.sleep(180)
+    graph.put_object(parent_object=poct, connection_name='comments',message=xx+npl)
+    print ("COMM3NT GY4 T3R4!\n")
+    time.sleep(170)
+    graph.put_object(parent_object=poct, connection_name='comments',message=cc+npl)
+    print ("COMM3NT GY4 T3R4!\n")
+    time.sleep(170)
+    graph.put_object(parent_object= poct, connection_name='comments',message=vv+npl)
+    print ("COMM3NT GY4 T3R4!\n")
+    time.sleep(170)
+    
+    graph.put_object(parent_object=poct, connection_name='comments',message=bb+npl)
+    print ("COMM3NT GY4 T3R4!\n")
+    time.sleep(170)
+    graph.put_object(parent_object=poct, connection_name='comments',message=nn+npl)
+    print ("COMM3NT GY4 T3R4!\n")
+    time.sleep(170)
+    graph.put_object(parent_object=poct, connection_name='comments',message=mm+npl)
+    print ("COMM3NT GY4 T3R4!\n")
+    time.sleep(170)
+    graph.put_object(parent_object= poct, connection_name='comments',message=ww+npl)
+    print ("COMM3NT GY4 T3R4!\n")
+    time.sleep(170)
+    
+    graph.put_object(parent_object=poct, connection_name='comments',message=ee+npl)
+    print ("COMM3NT GY4 T3R4!\n")
+    time.sleep(170)
+    graph.put_object(parent_object=poct, connection_name='comments',message=tt+npl)
+    print ("COMM3NT GY4 T3R4!\n")
+    time.sleep(170)
+    graph.put_object(parent_object=poct, connection_name='comments',message=yy+npl)
+    print ("COMM3NT GY4 T3R4!\n")
+    time.sleep(179)
+
+    
+if __name__ == '__main__':
+    auto_post()
+    print ("COMM3NT GY4 T3R4!\n")
+#YOUR D9DDY 9BHII SINGH9NIY9 H3R3 <3 8|
